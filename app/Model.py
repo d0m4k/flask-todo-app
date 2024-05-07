aa1e20af3d596c0b57ef495662dba49b56db7993
PhyoeKyawThhan <kyawthan.phyoe0@gmail.com>
2024-05-07 15:58:52 +0630

    change: sqlite3 to mysql connector

--- app/Model.py
+++ app/Model.py
@@ -1,27 +1,34 @@
-import sqlite3
+from mysql import connector
 from datetime import datetime
 class Model:
     def __init__(self):
-        self.connect = sqlite3.connect("todo.db")
+        self.connect = connector.connect(
+            host="localhost",
+            user="domak",
+            password="audrey",
+            database="test"
+        )
         self.cursor = self.connect.cursor()
-        self.cursor.execute("""create table if not exists 
-                            todo(
-                            id integer primary key autoincrement,
-                            user_id integer not null,
-                            text varchar(200),
-                            date Date,
-                            foreign key(user_id) references user(id))""")
         self.cursor.execute("""create table if not exists
-                            user(id integer primary key autoincrement,
+                            user(id integer primary key auto_increment,
                             username varchar(20), 
                             email varchar(100),
                             password varchar(200),
                             created_time Date
                             )""")
+        self.cursor.execute("""create table if not exists 
+                            todo(
+                            id integer primary key auto_increment,
+                            user_id integer not null,
+                            text varchar(200),
+                            date Date,
+                            foreign key(user_id) references user(id))""")
         self.connect.commit()
 
-    def insert(self, text, user_id):
-        self.cursor.execute(f"insert into todo(user_id, text, date) values({user_id},'{text}', '{str(datetime.utcnow())}')")
+    def insert(self, text, user_id): # it's
+        query = "insert into todo(user_id, text, date) values(%s, %s, %s)"
+        value = (user_id, text, str(datetime.utcnow()))
+        self.cursor.execute(query, value)
         self.connect.commit()
     
     def update(self, todo_id, edit_text):
@@ -33,9 +40,10 @@ class Model:
         self.connect.commit()
 
     def viewTodos(self):
-        todos = self.cursor.execute("""select todo.*, user.username
+        self.cursor.execute("""select todo.*, user.username
                                      from todo, user 
-                                    where user.id = todo.user_id""").fetchall()
+                                    where user.id = todo.user_id""")
+        todos = self.cursor.fetchall()
         return todos
     
     def createUser(self, username, email, password):
@@ -47,21 +55,23 @@ class Model:
                                 '{password}',
                                 '{str(datetime.utcnow())}')""")
         self.connect.commit()
-        return self.cursor.execute(f"""select * from user 
+        self.cursor.execute(f"""select * from user 
                                    where email='{email}' 
                                    and 
-                                   password='{password}'""").fetchone()
+                                   password='{password}'""")
+        return self.cursor.fetchone()
     
     def deleteUser(self, user_id):
         self.cursor.execute(f"delete from user where id={user_id}")
         self.connect.commit()
     
     def user(self, email, password):
-        return self.cursor.execute(f"""select 
+        self.cursor.execute(f"""select 
                             * from user
                             where email='{email}'
                             and password='{password}'
-                            """).fetchone()
+                            """)
+        return self.cursor.fetchone()
     # close connection
     def close(self):
         self.cursor.close()
